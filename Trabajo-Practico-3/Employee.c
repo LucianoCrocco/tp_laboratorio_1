d2c26659cf1732ffc8472ae1fb58dc68d693b6bf
LucianoCrocco <lucianoacrocco@gmail.com>
2021-06-06 00:14:14 -0300

    Setters y Getters hechos

---
+++ Trabajo-Practico-3/Employee.c
@@ -0,0 +1,123 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "LinkedList.h"
+#include "Employee.h"
+
+Employee* employee_new(){
+
+	Employee* newEmployee;
+
+	newEmployee = (Employee*) malloc (sizeof(Employee));
+
+	if(newEmployee != NULL){
+
+		employee_setId(newEmployee, 0);
+		employee_setNombre(newEmployee, " ");
+		employee_setHorasTrabajadas(newEmployee, 0);
+		employee_setSueldo(newEmployee, 0);
+	}
+
+	return newEmployee;
+}
+Employee* employee_newParametros(char* idStr,char* nombreStr,char* horasTrabajadasStr, char* sueldoStr){
+	Employee* newEmployee;
+
+	newEmployee = employee_new();
+
+	if(newEmployee != NULL){
+
+		employee_setId(newEmployee, atoi(idStr));
+		employee_setNombre(newEmployee, nombreStr);
+		employee_setHorasTrabajadas(newEmployee, atoi(horasTrabajadasStr));
+		employee_setSueldo(newEmployee, atoi(sueldoStr));
+	}
+
+	return newEmployee;
+}
+
+void employee_delete(Employee* this){
+	if(this != NULL){
+		free(this);
+	}
+}
+
+int employee_setId(Employee* this,int id){
+
+	if(this != NULL && id >= 0){
+		this->id = id;
+		return 1;
+	} else {
+		return 0;
+	}
+}
+int employee_getId(Employee* this,int* id){
+
+	if(this != NULL && id >= 0){
+		*id = this->id;
+		return 1;
+	} else {
+		return 0;
+	}
+}
+
+int employee_setNombre(Employee* this,char* nombre){
+
+	if(this != NULL && nombre != NULL){
+		strcpy(this->nombre, nombre);
+		return 1;
+	} else {
+		return 0;
+	}
+
+}
+int employee_getNombre(Employee* this,char* nombre){
+
+	if(this != NULL && nombre != NULL){
+		strcpy(nombre, this->nombre);
+		return 1;
+	} else {
+		return 0;
+	}
+
+}
+
+int employee_setHorasTrabajadas(Employee* this,int horasTrabajadas){
+
+	if(this != NULL && horasTrabajadas >= 0){
+		this->horasTrabajadas = horasTrabajadas;
+		return 1;
+	} else {
+		return 0;
+	}
+
+}
+int employee_getHorasTrabajadas(Employee* this,int* horasTrabajadas){
+
+	if(this != NULL && horasTrabajadas >= 0){
+		*horasTrabajadas = this->horasTrabajadas;
+		return 1;
+	} else {
+		return 0;
+	}
+}
+
+int employee_setSueldo(Employee* this,int sueldo){
+
+	if(this != NULL && sueldo >= 0){
+		this->sueldo = sueldo;
+		return 1;
+	} else {
+		return 0;
+	}
+
+}
+int employee_getSueldo(Employee* this,int* sueldo){
+
+	if(this != NULL && sueldo >= 0){
+		*sueldo = this->sueldo;
+		return 1;
+	} else {
+		return 0;
+	}
+}
---
+++ Trabajo-Practico-3/parser.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "LinkedList.h"
+#include "Employee.h"
+
+/** \brief Parsea los datos los datos de los empleados desde el archivo data.csv (modo texto).
+ *
+ * \param path char*
+ * \param pArrayListEmployee LinkedList*
+ * \return int
+ *
+ */
+int parser_EmployeeFromText(FILE* pFile , LinkedList* pArrayListEmployee)
+{
+	Employee* auxEmployee;
+
+	char id[128];
+	char nombre[128];
+	char horasTrabajadas[128];
+	char sueldo[128];
+
+	int flag = 0;
+
+	if(pFile != NULL){
+
+		while(!feof(pFile) && pArrayListEmployee != NULL){
+			if(flag == 0){
+				fscanf(pFile,"%[^,],%[^,],%[^,],%[^\n]\n", id, nombre, horasTrabajadas, sueldo);
+				flag ++;
+			}
+
+			fscanf(pFile,"%[^,],%[^,],%[^,],%[^\n]\n", id, nombre, horasTrabajadas, sueldo);
+
+			auxEmployee= employee_newParametros(id, nombre, horasTrabajadas, sueldo);
+
+			ll_add(pArrayListEmployee, auxEmployee);
+		}
+	} else {
+		return 0;
+	}
+
+	free(pFile);
+
+    return 1;
+}
+
+/** \brief Parsea los datos los datos de los empleados desde el archivo data.csv (modo binario).
+ *
+ * \param path char*
+ * \param pArrayListEmployee LinkedList*
+ * \return int
+ *
+ */
+int parser_EmployeeFromBinary(FILE* pFile , LinkedList* pArrayListEmployee)
+{
+	Employee* auxEmployee;
+
+	if(pFile != NULL){
+
+		while(!feof(pFile) && pArrayListEmployee != NULL){
+
+			fread(auxEmployee,sizeof(Employee),1,pFile);
+			ll_add(pArrayListEmployee, auxEmployee);
+
+		}
+	} else {
+		return 0;
+	}
+
+	free(pFile);
+    return 1;
+}
